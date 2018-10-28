b2da5a60375f1e50ad86c30b51987ce1b3e1b86d
ykisialiou <hiluro@gmail.com>
2018-10-29 02:06:37 +0300

    One more path fix

--- rsync-task/task.sh
+++ rsync-task/task.sh
@@ -12,5 +12,5 @@ fi
 # sed trick to format key properly
 echo $SSH_PRIVATE_KEY  | sed 's/-----BEGIN RSA PRIVATE KEY-----//' | sed 's/ -----END RSA PRIVATE KEY-----//' | sed 's/ /\n/g'  | sed '1s/^/-----BEGIN RSA PRIVATE KEY-----/' | sed -e '$a-----END RSA PRIVATE KEY-----' > ../server_key
 chmod 600 ../server_key
-rsync -e 'ssh -oStrictHostKeyChecking=no -i ../server_key' -qpr ../om-installation $USER@$SERVER:/home/$USER/backups/
+rsync -e 'ssh -oStrictHostKeyChecking=no -i ../server_key' -qpr om-installation $USER@$SERVER:/home/$USER/backups/
 rm ../server_key
