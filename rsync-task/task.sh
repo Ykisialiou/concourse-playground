4e33740478cc7464cea14828908f6e052eb0a0b6
ykisialiou <hiluro@gmail.com>
2018-10-28 23:58:05 +0300

    Rsync sample task

---
+++ rsync-task/task.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+if [[ $DEBUG == true ]]; then
+  set -ex
+else
+  set -e
+fi
+
+# content of om-instalation sent to remote server
+
+echo $SSH_PRIVATE_KEY > server_key
+rsync -e 'ssh -i server_key' -qpr om-installation $USER@$SERVER:/home/$USER/backups/
+rm server_key
