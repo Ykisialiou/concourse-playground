400364fdf48c3e17a8f8fb1e49795a7a29c05ebc
ykisialiou <hiluro@gmail.com>
2018-10-29 01:49:49 +0300

    Format key

--- rsync-task/task.sh
+++ rsync-task/task.sh
@@ -9,7 +9,8 @@ fi
 
 # content of om-instalation sent to remote server
 
-echo $SSH_PRIVATE_KEY > ../server_key
+# sed trick to format key properly
+echo $SSH_PRIVATE_KEY  | sed 's/-----BEGIN RSA PRIVATE KEY-----//' | sed 's/ -----END RSA PRIVATE KEY-----//' | sed 's/ /\n/g'  | sed '1s/^/-----BEGIN RSA PRIVATE KEY-----/' | sed -e '$a-----END RSA PRIVATE KEY-----' > ../server_key
 chmod 600 ../server_key
 rsync -e 'ssh -oStrictHostKeyChecking=no -i server_key' -qpr ../om-installation $USER@$SERVER:/home/$USER/backups/
 rm ../server_key
