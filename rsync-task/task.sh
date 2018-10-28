5e419be7ab24588ddbc855de27b788a83f931fb0
ykisialiou <hiluro@gmail.com>
2018-10-29 01:06:52 +0300

    Fixes in path

--- rsync-task/task.sh
+++ rsync-task/task.sh
@@ -9,6 +9,7 @@ fi
 
 # content of om-instalation sent to remote server
 
-echo $SSH_PRIVATE_KEY > server_key
-rsync -e 'ssh -oStrictHostKeyChecking=no -i server_key' -qpr om-installation $USER@$SERVER:/home/$USER/backups/
-rm server_key
+echo $SSH_PRIVATE_KEY > ../server_key
+chmod 600 ../server_key
+rsync -e 'ssh -oStrictHostKeyChecking=no -i server_key' -qpr ../om-installation $USER@$SERVER:/home/$USER/backups/
+rm ../server_key
